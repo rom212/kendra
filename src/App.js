25c5b3267c200d26619fa9087cda9e6f40c4fe1b
romull <romull@amazon.com>
2020-06-19 13:43:06 -0400

    nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Nav from './nav/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+  
+  state = {
+    isLoggedIn: true,
+  }
+  
+  render() {
+
+    return (
+      <BrowserRouter>
+        <div className="App">
+            
+            <Nav authStatus={this.state.isLoggedIn} />
+            <h1 className="white dib">TEST RO</h1>
+            <h2> Please log in</h2>
+            {/* <Route exact path='/' component={Robosearch} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/signin' component={Signin} /> */}
+        </div>
+      </BrowserRouter> 
+
+
+    );
+  }
 }
 
 export default App;
